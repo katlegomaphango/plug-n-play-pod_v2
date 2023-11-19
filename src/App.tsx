6792ba5621a582b0eda1f4cfe07dbb8950714c83
Katlego Maphango <mphangokatlego08@gmail.com>
2023-11-19 22:05:35 +0200

    initial login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Navbar } from './components'
+import { Login } from './pages'
 
 function App() {
 
   return (
     <>
       <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Login />} />
+      </Routes>
       <div>Hey</div>
     </>
   )
---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+import Login from "./LoginPage";
+
+export {
+    Login
+}
